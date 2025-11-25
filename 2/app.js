df6f4b11d5c10b4c97e9fb34e4b7bdde4293494d
Vinayakjava21 <vinayakjava21@gmail.com>
2025-11-25 11:28:50 +0530

    Added today's project into folder 2

---
+++ 2/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+console.log("react")
+// const header = React.createElement("h1",{},"Welcome 21");
+ const root  = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(header);
+
+
+const parent = React.createElement("div", {},
+    React.createElement("div", {},
+        [React.createElement("h1", {}, "H1"), React.createElement("h2", {}, "H2")])
+);
+
+root.render(parent);
